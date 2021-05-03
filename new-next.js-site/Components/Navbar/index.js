4fe00d7253d4e2e86299fdbd2342e9fe74606a2c
nick adams <thebigdeal916@icloud.com>
2021-05-03 17:01:02 -0500

    folder structure and navbar added

---
+++ new-next.js-site/Components/Navbar/index.js
@@ -0,0 +1,26 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import styles from './style.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => (
+    <>
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home" id={styles.nav}>Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-light">Search</Button>
+    </Form>
+  </Navbar>
+</>
+)
+
+export default NavBar;
---
+++ new-next.js-site/Components/Wrapper/index.js
@@ -0,0 +1,8 @@
+import Navbar from '../Navbar'
+
+const Wrapper = () => (
+    <Navbar />
+
+)
+
+export default Wrapper;
